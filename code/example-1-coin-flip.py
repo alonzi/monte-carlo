5686da3f437aab5680effd06e059b83edec32381
pete <lpa2a@virginia.edu>
2022-02-26 22:14:23 -0500

    add flip simulation plot

--- code/example-1-coin-flip.py
+++ code/example-1-coin-flip.py
@@ -1,6 +1,10 @@
 import numpy as np
+import matplotlib.pyplot as plt
 
-N = 10
+N = 10**1
+# N = 10**3
+# N = 10**6
+avg = []
 
 def toCoin(x):
     if x==0: return 'heads'
@@ -14,3 +18,14 @@ print('Coin Flips = ',list(map(toCoin,results)))
 print('# of heads =',sum(results))
 print('# of tails =', N-sum(results))
 print('\n')
+
+for result in results:
+    if len(avg) == 0: avg.append(result)
+    else: avg.append((avg[len(avg)-1]*len(avg)+result) / (len(avg)+1) )
+
+plt.plot(avg)
+plt.axhline(y = 0.5, color = 'blue', linestyle = '-')
+plt.title("Percentage of Tails to Heads for N flips")
+plt.xlabel("Flips")
+plt.ylabel("Tails/Heads")
+plt.show()
